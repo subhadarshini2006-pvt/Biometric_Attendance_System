8914aaba24d5790fe99a114aaad301a4d3edb360
subhadarshini2006-pvt <subhadarshini.m2006@gmail.com>
2026-06-20 08:49:04 +0530

    Upload create_db.py

---
+++ create_db.py
@@ -0,0 +1,15 @@
+﻿import mysql.connector
+
+try:
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password=admin123""  # Leave blank if you don't use a password, or type it here
+    )
+    cursor = mydb.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS attendance_system")
+    print("🚀 Success! 'attendance_system' database is ready.")
+    cursor.close()
+    mydb.close()
+except Exception as e:
+    print(f"❌ Error connecting to MySQL: {e}")
